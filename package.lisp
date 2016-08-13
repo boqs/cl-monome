90a9d2dd4207d4f5d4db647c66bdaaac1ba84672
The Hairy One <sasquatch@rickvenn.com>
2016-08-13 14:05:24 -0700

    export some things necessary for serial-hub app

--- package.lisp
+++ package.lisp
@@ -1,5 +1,7 @@
 ;;;; package.lisp
 
 (defpackage #:cl-monome
-  (:use #:cl #:cffi))
-
+  (:use #:cl #:cffi)
+  (:export #:monome-led-level-set #:monome-open #:monome-event-get-grid
+	   #:monome-register-handler #:monome-close #:monome-event-loop
+	   #:monome-event-type-t #:monome-led-all))
